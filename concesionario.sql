4337d0b33db63c6fe003b9b517b215072166bdb5
CLavilla <cristianlavilla@hotmail.com>
2019-07-31 15:33:21 -0300

    mi primer commit

---
+++ concesionario.sql
@@ -0,0 +1,186 @@
+﻿CREATE DATABASE  IF NOT EXISTS `concesionario` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `concesionario`;
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `linea`
+--
+
+DROP TABLE IF EXISTS `linea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `linea` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `id_linea` int(11) NOT NULL,
+  `marca_id` bigint(20) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_98dhu64nvxxn63wh6ot68fyuy` (`id_linea`),
+  KEY `FK_h982an3cdvyiampv6mu07ab9u` (`marca_id`),
+  CONSTRAINT `FK_h982an3cdvyiampv6mu07ab9u` FOREIGN KEY (`marca_id`) REFERENCES `marca` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `linea`
+--
+
+LOCK TABLES `linea` WRITE;
+/*!40000 ALTER TABLE `linea` DISABLE KEYS */;
+INSERT INTO `linea` VALUES (1,0,1,1,'Spark');
+/*!40000 ALTER TABLE `linea` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `marca`
+--
+
+DROP TABLE IF EXISTS `marca`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `marca` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `id_marca` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_ril9j5diq030ss50crks5u2xm` (`id_marca`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `marca`
+--
+
+LOCK TABLES `marca` WRITE;
+/*!40000 ALTER TABLE `marca` DISABLE KEYS */;
+INSERT INTO `marca` VALUES (1,0,1,'Chevrolet');
+/*!40000 ALTER TABLE `marca` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `propietario`
+--
+
+DROP TABLE IF EXISTS `propietario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `propietario` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `apellidos` varchar(255) NOT NULL,
+  `celular` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `fecha_nacimiento` datetime NOT NULL,
+  `identificacion` bigint(20) NOT NULL,
+  `nombres` varchar(255) NOT NULL,
+  `tipo_id_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_8rceiw173456ueaaef3oyavr2` (`identificacion`),
+  KEY `FK_21nfj3neasqqs3l435pthy3te` (`tipo_id_id`),
+  CONSTRAINT `FK_21nfj3neasqqs3l435pthy3te` FOREIGN KEY (`tipo_id_id`) REFERENCES `tipo_identificacion` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `propietario`
+--
+
+LOCK TABLES `propietario` WRITE;
+/*!40000 ALTER TABLE `propietario` DISABLE KEYS */;
+INSERT INTO `propietario` VALUES (1,0,'Obando','456789','manuelobando15@gmail.com','1990-09-09 00:00:00',1085,'Manuel',2),(2,0,'Patiño','56381','mari','2014-09-09 00:00:00',1086,'Mario',1);
+/*!40000 ALTER TABLE `propietario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo_identificacion`
+--
+
+DROP TABLE IF EXISTS `tipo_identificacion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tipo_identificacion` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `id_tipo` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_xeapujp1mslsdauefuq5ek87` (`id_tipo`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_identificacion`
+--
+
+LOCK TABLES `tipo_identificacion` WRITE;
+/*!40000 ALTER TABLE `tipo_identificacion` DISABLE KEYS */;
+INSERT INTO `tipo_identificacion` VALUES (1,0,1,'Tarjeta Identidad'),(2,0,2,'Cedula de Ciudadania'),(3,0,3,'Cedula de Extranjeria');
+/*!40000 ALTER TABLE `tipo_identificacion` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehiculo`
+--
+
+DROP TABLE IF EXISTS `vehiculo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehiculo` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `version` bigint(20) NOT NULL,
+  `chasis` varchar(255) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `linea_id` bigint(20) NOT NULL,
+  `marca_id` bigint(20) NOT NULL,
+  `modelo` varchar(255) NOT NULL,
+  `placa` varchar(255) NOT NULL,
+  `propietario_id` bigint(20) NOT NULL,
+  `transmicion` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_qt70ab7dm6c22avc6b4koit9c` (`placa`),
+  KEY `FK_llbkk84brqeukh6ydd58lwdxs` (`linea_id`),
+  KEY `FK_hjo402w8ya9qw0f8wtkwger5y` (`marca_id`),
+  KEY `FK_mbyhyailda64oh6cxr2qeg5db` (`propietario_id`),
+  CONSTRAINT `FK_hjo402w8ya9qw0f8wtkwger5y` FOREIGN KEY (`marca_id`) REFERENCES `marca` (`id`),
+  CONSTRAINT `FK_llbkk84brqeukh6ydd58lwdxs` FOREIGN KEY (`linea_id`) REFERENCES `linea` (`id`),
+  CONSTRAINT `FK_mbyhyailda64oh6cxr2qeg5db` FOREIGN KEY (`propietario_id`) REFERENCES `propietario` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehiculo`
+--
+
+LOCK TABLES `vehiculo` WRITE;
+/*!40000 ALTER TABLE `vehiculo` DISABLE KEYS */;
+INSERT INTO `vehiculo` VALUES (1,0,'ahfkahs','Rojo','img',1,1,'1998','AUO-836',2,'Manual');
+/*!40000 ALTER TABLE `vehiculo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'concesionario'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
